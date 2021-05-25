8f3ed9cece3364620f62a411778762e6f3eeefda
Chaturya567 <74906960+Chaturya567@users.noreply.github.com>
2021-05-25 11:52:07 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,157 @@
+/* calculator in c language, can be used to test (or) your works also, the code is open so you can use git*/
+
+/* functions are add, subtract,multiply, divide, remainder*
+
+#include <stdio.h>
+
+int main() {
+
+printf("Welcome To Calculator, \n Type A For Addition\n Type B For Subtraction\n Type M For Multiply \n Type D For Divide\n Type R For Reminder \n Type E To Exit \n \n Input: ");
+
+char useroption, option;
+
+scanf("%c", &useroption);
+
+option =toLower(useroption);
+
+if (option == 'a') {
+
+printf("How Many Numbers You Want To Add? \n Input: ");
+
+int times, i;
+
+scanf("%d", &times);
+
+if (times <= 1) {
+printf(" Enter Atleast 2 Numbers*);
+}
+
+for (i = 0; i < times; i++) {
+
+float digit;
+
+printf("Type A Number: ");
+
+scanf("%f", &digit);
+
+float result = digit;
+
+result = result + digit;
+continue;
+
+}
+
+printf(" The Sum Is: %f\n", result);
+
+
+} else if (option == 's') {
+
+printf("How Many Numbers You Want To Subtract? \n Input: ");
+
+int times, i;
+
+scanf("%d", &times);
+
+if (times <= 1) {
+printf(" Enter Atleast 2 Numbers*);
+}
+
+for (i = 0; i < times; i++) {
+
+float digit;
+
+printf("Type A Number: ");
+
+scanf("%f", &digit);
+
+float result = digit;
+
+result = result - digit;
+}
+
+printf(" The Result Is: %f\n", result);
+
+} else if (option == 'm') {
+
+printf("How Many Numbers You Want To Multiply? \n Input: ");
+
+int times, i;
+
+
+scanf("%d", &times);
+
+if (times <= 1) {
+printf(" Enter Atleast 2 Numbers*);
+}
+
+for (i = 0; i < times; i++) {
+
+float digit;
+
+printf("Type A Number: ");
+
+scanf("%f", &digit);
+
+float result = digit;
+
+result = result * digit;
+}
+printf(" The Result Is: %f\n", result);
+} else if (option == 'd') {
+
+printf("How Many Numbers You Want To Divide? \n Input: ");
+
+int times, i;
+
+scanf("%d", &times);
+
+if (times <= 1) {
+printf(" Enter Atleast 2 Numbers*);
+}
+
+for (i = 0; i < times; i++) {
+
+float digit;
+
+printf("Type A Number: ");
+
+scanf("%f", &digit);
+
+float result = digit;
+
+result = result / digit;
+}
+printf(" Result: %f\n", result);
+} else if (option == 'r') {
+printf("How Many Numbers You Want To Add? \n Input: ");
+
+int times, i;
+
+scanf("%d", &times);
+
+if (times <= 1) {
+printf(" Enter Atleast 2 Numbers*);
+}
+
+for (i = 0; i < times; i++) {
+
+float digit;
+
+printf("Type A Number: ");
+
+scanf("%f", &digit);
+
+float result = digit;
+
+result = result / digit;
+}
+printf(" The Reminder Would Be: %f\n", result);
+} else if (option == 'e') {
+
+printf("Bye Have A Great Time!!!\n");
+
+} else {
+printf("Invalid Option, Program End!!! \n");
+}
+return 0;
+}
